84b3fecf76b9dff9f23ed7327bddab30568257dc
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-12-10 10:38:44 -0300

    up

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './pages/login';
+import Register from './pages/register';
+import reportWebVitals from './reportWebVitals';
+
+import {FirebaseAppProvider} from 'reactfire';
+import firebaseConfig from './utils/firebaseConfig';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Register />
+    </FirebaseAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA6691ONs5iVichyrXw-eVb93kqG3wMVHc",
+    authDomain: "nyous-tarde.firebaseapp.com",
+    projectId: "nyous-tarde",
+    storageBucket: "nyous-tarde.appspot.com",
+    messagingSenderId: "772871087957",
+    appId: "1:772871087957:web:7fac0c9d221b9e82ac31dc"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
